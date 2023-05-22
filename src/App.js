167b1bb3bc1f7030c3b8bf07e356223663890b17
EmirSoltonbek <emirsoltonbekov7@gmail.com>
2023-05-22 20:24:02 +0600

    .

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import MyRoutes from "./MyRoutes";
 import Footer from "./components/Footer/Footer";
+import ProductCard from "./components/Prouduct/productCard/ProductCard";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <ProductCard />
       <Footer />
     </div>
   );
