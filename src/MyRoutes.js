bbf20a1c7a582054576c7705a8ea043ac55aff6d
m3marlen <marlenarzymatov87924@gmail.com>
2023-05-24 21:05:15 +0300

    .

--- src/MyRoutes.js
+++ src/MyRoutes.js
@@ -6,6 +6,8 @@ import ProductListPage from "./pages/ProductListPage";
 import HomePage from "./pages/HomePage";
 import AddProductPage from "./pages/AddProductPage";
 import Login from "./components/auth/Login";
+import ProfilePage from "./pages/ProfilePage";
+import InstProfile from "./components/profile/InstProfile";
 
 function MyRoutes() {
   const PUBLIC_ROUTES = [
@@ -15,6 +17,8 @@ function MyRoutes() {
     { link: "/chat", element: <ChatPage />, id: 4 },
     { link: "/register", element: <RegisterFormPage />, id: 5 },
     { link: "/login", element: <Login />, id: 6 },
+    { link: "/profile", element: <ProfilePage />, id: 7 },
+    { link: "/inst-profile", element: <InstProfile />, id: 8 },
   ];
   return (
     <div>
---
+++ src/pages/InstProfilePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function InstProfilePage() {
+  return (
+    <div>
+      <InstProfile />
+    </div>
+  );
+}
+
+export default InstProfilePage;
