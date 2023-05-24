84dc668f582f080659e6eae0b2cbf0c18cb935d8
m3marlen <marlenarzymatov87924@gmail.com>
2023-05-24 11:15:54 +0300

    register&login almost done

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,26 +1,24 @@
 import React, { useState } from "react";
-import "./RegisterForm.css";
-import { useAuth } from "../../../contexts/AuthContextProvider";
-import Loader from "../../Loader";
+import "../auth/registerForm/RegisterForm.css";
+import { useAuth } from "../../contexts/AuthContextProvider";
+import Loader from "../Loader";
 
-const RegisterForm = () => {
+const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passwordConfirm, setPasswordConfirm] = useState("");
   const [phone, setPhone] = useState(0);
 
-  const { handleRegister, loading, error, setError } = useAuth();
+  const { handleLogin, loading, error, setError } = useAuth();
 
   function handleSave() {
-    if (!email.trim() || !password.trim() || !passwordConfirm.trim()) {
+    if (!email.trim() || !password.trim()) {
       alert("заполните поля");
     } else {
       let formData = new FormData();
       formData.append("email", email);
       formData.append("password", password);
-      formData.append("password_confirm", passwordConfirm);
-      formData.append("phone", phone);
-      handleRegister(formData);
+      handleLogin(formData, email);
     }
   }
 
@@ -35,7 +33,7 @@ const RegisterForm = () => {
           <div className="circle"></div>
           <div className="register-form-container">
             <form action="">
-              <h1>Register</h1>
+              <h1>Login</h1>
               {error ? <h2>{error}</h2> : null}
               <div className="form-fields">
                 <div className="form-field">
@@ -55,7 +53,7 @@ const RegisterForm = () => {
                     required
                   />
                 </div>
-                <div className="form-field">
+                {/* <div className="form-field">
                   <input
                     type="password"
                     onChange={(e) => setPasswordConfirm(e.target.value)}
@@ -64,19 +62,19 @@ const RegisterForm = () => {
                     minlength="8"
                     maxlength="128"
                   />
-                </div>
-                <div className="form-field">
+                </div> */}
+                {/* <div className="form-field">
                   <input
                     type="number"
                     onChange={(e) => setPhone(e.target.value)}
                     placeholder="phone"
                     required
                   />
-                </div>
+                </div> */}
               </div>
               <div className="form-buttons">
                 <button onClick={handleSave} className="button">
-                  register
+                  login
                 </button>
                 <div className="divider">или</div>
                 <a href="#" className="button button-google">
@@ -91,4 +89,4 @@ const RegisterForm = () => {
   );
 };
 
-export default RegisterForm;
+export default Login;
