ec0b91cc8340c4b8d752740e038c5b82ee37f722
slartibartfast <slartibartfast1998@gmail.com>
2023-05-25 20:10:06 +0600

    cart verstka

--- src/MyRoutes.js
+++ src/MyRoutes.js
@@ -6,12 +6,11 @@ import ProductListPage from "./pages/ProductListPage";
 import HomePage from "./pages/HomePage";
 import AddProductPage from "./pages/AddProductPage";
 import Login from "./components/auth/Login";
-<<<<<<< HEAD
+
 import ProfilePage from "./pages/ProfilePage";
 import InstProfile from "./components/profile/InstProfile";
-=======
+
 import EditProductPage from "./pages/EditProductPage";
->>>>>>> 75bae3a9b549d914d03e422b3b06a4ed75fbe900
 import CartPage from "./pages/CartPage";
 
 function MyRoutes() {
@@ -22,12 +21,9 @@ function MyRoutes() {
     { link: "/chat", element: <ChatPage />, id: 4 },
     { link: "/register", element: <RegisterFormPage />, id: 5 },
     { link: "/login", element: <Login />, id: 6 },
-<<<<<<< HEAD
     { link: "/profile", element: <ProfilePage />, id: 7 },
     { link: "/inst-profile", element: <InstProfile />, id: 8 },
-=======
     { link: "/edit/:id", element: <EditProductPage />, id: 7 },
->>>>>>> 75bae3a9b549d914d03e422b3b06a4ed75fbe900
     { link: "cart", element: <CartPage />, id: 8 },
   ];
   return (
