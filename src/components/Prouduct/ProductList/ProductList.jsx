192a3237af5f118760f426d597125b3c91c645b1
slartibartfast <slartibartfast1998@gmail.com>
2023-06-02 15:43:00 +0600

    13

--- src/components/Prouduct/ProductList/ProductList.jsx
+++ src/components/Prouduct/ProductList/ProductList.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { Pagination } from "react-bootstrap";
-// import { useSearchParams } from "react-router-dom";
 import ProductCard from "../productCard/ProductCard";
 import { useProduct } from "../../../contexts/ProductContextProvider";
 import { useSearchParams } from "react-router-dom";
@@ -9,7 +8,6 @@ const ProductList = () => {
   const { getProducts, products, pages } = useProduct();
 
   const [searchParams, setSearchParams] = useSearchParams();
-  // console.log(products);
 
   const [currentPage, setCurrentPage] = useState(1);
   function getPagesCount() {
@@ -40,10 +38,6 @@ const ProductList = () => {
               return <ProductCard key={item.id} item={item} />;
             })
           : null}
-        {/* {products?.map((item) => {
-
-          return <ProductCard key={item.id} item={item} />;
-        })} */}
       </div>
       <Pagination>
         <Pagination.Prev onClick={() => setCurrentPage(currentPage - 1)} />
@@ -69,3 +63,63 @@ const ProductList = () => {
 };
 
 export default ProductList;
+// //
+// const ProductList = () => {
+//   const { getProducts, products, pages } = useProduct();
+//   const [searchParams, setSearchParams] = useSearchParams();
+//   const [currentPage, setCurrentPage] = useState(1);
+
+//   function getPagesCount() {
+//     const pageCountArr = [];
+//     for (let i = 1; i <= pages; i++) {
+//       pageCountArr.push(i);
+//     }
+//     return pageCountArr;
+//   }
+
+//   useEffect(() => {
+//     setSearchParams({ page: currentPage });
+//   }, [currentPage, setSearchParams]);
+
+//   useEffect(() => {
+//     getProducts();
+//   }, [searchParams, getProducts]);
+
+//   if (currentPage < 1) {
+//     setCurrentPage(1);
+//   } else if (currentPage > pages) {
+//     setCurrentPage(pages);
+//   }
+
+//   return (
+//     <div>
+//       <h1>PRODUCT LIST</h1>
+//       <div style={{ display: "flex", flexWrap: "wrap" }}>
+//         {products.length
+//           ? products.map((item) => <ProductCard key={item.id} item={item} />)
+//           : null}
+//       </div>
+//       <Pagination>
+//         <Pagination.Prev onClick={() => setCurrentPage(currentPage - 1)} />
+//         {getPagesCount().map((item) =>
+//           item === currentPage ? (
+//             <Pagination.Item
+//               onClick={() => setCurrentPage(item)}
+//               key={item}
+//               active
+//             >
+//               {item}
+//             </Pagination.Item>
+//           ) : (
+//             <Pagination.Item onClick={() => setCurrentPage(item)} key={item}>
+//               {item}
+//             </Pagination.Item>
+//           )
+//         )}
+//         <Pagination.Next onClick={() => setCurrentPage(currentPage + 1)} />
+//       </Pagination>
+//     </div>
+//   );
+// };
+
+// export default ProductList;
