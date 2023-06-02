0343d7ed64fc98b26005e23cd6ce5447f30d3948
slartibartfast <slartibartfast1998@gmail.com>
2023-06-02 16:17:38 +0600

    verstka emira categii

--- src/components/Prouduct/ProductList/ProductList.jsx
+++ src/components/Prouduct/ProductList/ProductList.jsx
@@ -22,6 +22,8 @@ const ProductList = () => {
     setSearchParams({ page: currentPage });
   }, [currentPage]);
 
+  // function
+
   useEffect(() => {
     getProducts();
   }, [searchParams]);
@@ -31,7 +33,6 @@ const ProductList = () => {
 
   return (
     <div>
-<<<<<<< HEAD
       <h1>PRODUCT LIST</h1>
       <div style={{ display: "flex", flexWrap: "wrap" }}>
         {products.length
@@ -39,110 +40,55 @@ const ProductList = () => {
               return <ProductCard key={item.id} item={item} />;
             })
           : null}
-=======
-      <h1 style={{textAlign:"center", margin:"20px"}}>PRODUCT LIST</h1>
-      <div style={{ display: "flex", flexWrap: "wrap", justifyContent:"space-evenly" }}>
-        {products?.map((item) => {
-          // return (genderCategory === item.gender || category === item.category) ? (
-          //   <ProductCard key={item.id} item={item} />
-          // ) : null;
+        <h1 style={{ textAlign: "center", margin: "20px" }}>PRODUCT LIST</h1>
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "space-evenly",
+          }}
+        >
+          {products?.map((item) => {
+            // return (genderCategory === item.gender || category === item.category) ? (
+            //   <ProductCard key={item.id} item={item} />
+            // ) : null;
 
-          // if (genderCategory === "all") {
-          //   return genderCategory === item.gender ||
-          //     category === item.category ? (
-          //     <ProductCard key={item.id} item={item} />
-          //   ) : null;
-          // } else {
-          //   return genderCategory === item.gender &&
-          //     category === item.category ? (
-          //     <ProductCard key={item.id} item={item} />
-          //   ) : null;
-          // }
-          return <ProductCard key={item.id} item={item} />;
-        })}
->>>>>>> f67d7c9203053d5b1cd39817bcfdec705893097c
+            // if (genderCategory === "all") {
+            //   return genderCategory === item.gender ||
+            //     category === item.category ? (
+            //     <ProductCard key={item.id} item={item} />
+            //   ) : null;
+            // } else {
+            //   return genderCategory === item.gender &&
+            //     category === item.category ? (
+            //     <ProductCard key={item.id} item={item} />
+            //   ) : null;
+            // }
+            return <ProductCard key={item.id} item={item} />;
+          })}
+        </div>
+        <Pagination style={{ justifyContent: "center", margin: "10px" }}>
+          <Pagination.Prev onClick={() => setCurrentPage(currentPage - 1)} />
+          {getPagesCount().map((item) =>
+            item === currentPage ? (
+              <Pagination.Item
+                onClick={() => setCurrentPage(item)}
+                key={item}
+                active
+              >
+                {item}
+              </Pagination.Item>
+            ) : (
+              <Pagination.Item onClick={() => setCurrentPage(item)} key={item}>
+                {item}
+              </Pagination.Item>
+            )
+          )}
+          <Pagination.Next onClick={() => setCurrentPage(currentPage + 1)} />
+        </Pagination>
       </div>
-      <Pagination style={{justifyContent:"center", margin:"10px"}}>
-        <Pagination.Prev onClick={() => setCurrentPage(currentPage - 1)} />
-        {getPagesCount().map((item) =>
-          item === currentPage ? (
-            <Pagination.Item
-              onClick={() => setCurrentPage(item)}
-              key={item}
-              active
-            >
-              {item}
-            </Pagination.Item>
-          ) : (
-            <Pagination.Item onClick={() => setCurrentPage(item)} key={item}>
-              {item}
-            </Pagination.Item>
-          )
-        )}
-        <Pagination.Next onClick={() => setCurrentPage(currentPage + 1)} />
-      </Pagination>
     </div>
   );
 };
 
 export default ProductList;
-// //
-// const ProductList = () => {
-//   const { getProducts, products, pages } = useProduct();
-//   const [searchParams, setSearchParams] = useSearchParams();
-//   const [currentPage, setCurrentPage] = useState(1);
-
-//   function getPagesCount() {
-//     const pageCountArr = [];
-//     for (let i = 1; i <= pages; i++) {
-//       pageCountArr.push(i);
-//     }
-//     return pageCountArr;
-//   }
-
-//   useEffect(() => {
-//     setSearchParams({ page: currentPage });
-//   }, [currentPage, setSearchParams]);
-
-//   useEffect(() => {
-//     getProducts();
-//   }, [searchParams, getProducts]);
-
-//   if (currentPage < 1) {
-//     setCurrentPage(1);
-//   } else if (currentPage > pages) {
-//     setCurrentPage(pages);
-//   }
-
-//   return (
-//     <div>
-//       <h1>PRODUCT LIST</h1>
-//       <div style={{ display: "flex", flexWrap: "wrap" }}>
-//         {products.length
-//           ? products.map((item) => <ProductCard key={item.id} item={item} />)
-//           : null}
-//       </div>
-//       <Pagination>
-//         <Pagination.Prev onClick={() => setCurrentPage(currentPage - 1)} />
-//         {getPagesCount().map((item) =>
-//           item === currentPage ? (
-//             <Pagination.Item
-//               onClick={() => setCurrentPage(item)}
-//               key={item}
-//               active
-//             >
-//               {item}
-//             </Pagination.Item>
-//           ) : (
-//             <Pagination.Item onClick={() => setCurrentPage(item)} key={item}>
-//               {item}
-//             </Pagination.Item>
-//           )
-//         )}
-//         <Pagination.Next onClick={() => setCurrentPage(currentPage + 1)} />
-//       </Pagination>
-//     </div>
-//   );
-// };
-
-// export default ProductList;
